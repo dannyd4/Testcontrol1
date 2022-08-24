9593f2f924d845c5305a54dc907b6c7dbe5c23ff
Danny_D <tip505@mail.ru>
2022-08-24 09:34:57 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Контрольная работа
+
+//Первый шаг создаём массив данных с типом строка
+Console.WriteLine("Введите элементы массива типа string");
+string[] array = new string[4];
+for (int i = 0; i < 4; i++)
+{
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "]");
+
+//Второй шаг проверяем длину каждого элемента созданного массива и выводим в форме нового массива.
+Console.WriteLine();
+
+
+int n = 0;
+for (int i = 0; i < 4; i++)
+{
+    if (array[i].Length < 4)
+    {
+        n++;
+    }
+}
+
+
+string[] ArrayNew = new string[n];
+
+int j = 0;
+for (int i = 0; i < 4; i++)
+{
+    if (array[i].Length < 4)
+    {
+        ArrayNew[j] = array[i];
+        j++;
+    }
+}
+
+Console.Write("[" + string.Join(", ", ArrayNew) + "]");
